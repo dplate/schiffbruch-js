904158d9dc6b75723699bc01eb3caa41ab4c747d
Dirk Plate <email@dplate.de>
2023-10-16 20:07:23 +0200

    Improve screen scaling

--- src/images/resizeCanvases.js
+++ src/images/resizeCanvases.js
@@ -5,20 +5,12 @@ const resizeCanvases = async (screenWidth, screenHeight) => {
   if (canvases.PRIMARY) {
     const primaryCanvas = canvases.PRIMARY.canvas;
     const optimalScaleFactor = Math.min(screenWidth / 800, screenHeight / 600);
-    if (optimalScaleFactor > 1) {
-      primaryCanvas.width = screenWidth;
-      primaryCanvas.height = screenHeight;
-      primaryCanvas.style.transform = '';
-      primaryCanvas.dataset.scaling = 1;
-      //canvases.PRIMARY.scale(Math.floor(optimalScaleFactor), Math.floor(optimalScaleFactor));
-    } else {
-      canvases.PRIMARY.scale(1, 1);
-      primaryCanvas.width = 800;
-      primaryCanvas.height = 600;
-      primaryCanvas.style.transform = `scale(${optimalScaleFactor})`;
-      primaryCanvas.style.transformOrigin = 'top left';
-      primaryCanvas.dataset.scaling = optimalScaleFactor;
-    }
+    const scaleFactor = optimalScaleFactor > 1 ? Math.floor(optimalScaleFactor) : optimalScaleFactor;
+    primaryCanvas.width = Math.max(800, screenWidth / scaleFactor);
+    primaryCanvas.height = Math.max(600, screenHeight / scaleFactor);
+    primaryCanvas.style.transform = `scale(${scaleFactor})`;
+    primaryCanvas.style.transformOrigin = 'top left';
+    primaryCanvas.dataset.scaling = scaleFactor;
     state.camera.width = primaryCanvas.width;
     state.camera.height = primaryCanvas.height;
   }
