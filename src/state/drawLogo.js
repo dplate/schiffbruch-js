f76bb1520398ed9f952df00685a001cbb2dc9069
Dirk Plate <email@dplate.de>
2023-11-11 15:53:25 +0100

    Fix size of logo on small screens

--- src/state/drawLogo.js
+++ src/state/drawLogo.js
@@ -2,24 +2,27 @@ import canvases from '../images/canvases.js';
 import images from '../images/images.js';
 import sounds from '../sounds/sounds.js';
 
-const width = 500;
-const height = 500;
-
 const drawLogo = () => {
   canvases.PRIMARY.fillStyle = `rgba(0, 0, 0, 1)`;
   canvases.PRIMARY.fillRect(0, 0, canvases.PRIMARY.canvas.width, canvases.PRIMARY.canvas.height);
+
+  const size = Math.min(500, canvases.PRIMARY.canvas.width - 50, canvases.PRIMARY.canvas.height - 50);
+ 
+  canvases.PRIMARY.imageSmoothingEnabled = true;
 
   canvases.PRIMARY.drawImage(
     images.LOGO.instance,
     0, 
     0, 
-    width, 
-    height,
-    Math.round((canvases.PRIMARY.canvas.width - width) / 2), 
-    Math.round((canvases.PRIMARY.canvas.height - height) / 2), 
-    width, 
-    height
+    500, 
+    500,
+    Math.round((canvases.PRIMARY.canvas.width - size) / 2), 
+    Math.round((canvases.PRIMARY.canvas.height - size) / 2), 
+    size, 
+    size
   );
+
+  canvases.PRIMARY.imageSmoothingEnabled = false;
 
   sounds.LOGO.instance.play(true);
 }
