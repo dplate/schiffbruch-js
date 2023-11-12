e93c2e30321eb51521398b50e374d36f52b048fa
Dirk Plate <email@dplate.de>
2023-11-12 13:12:00 +0100

    Avoid waves outside of tile

--- src/terrain/objects/createWaves.js
+++ src/terrain/objects/createWaves.js
@@ -1,13 +1,16 @@
 import spriteTypes from '../../images/spriteTypes.js';
 import sprites from '../../images/sprites.js';
 
-const createWaves = () => ({
-  sprite: spriteTypes.WAVES,
-  x: Math.floor(Math.random() * 5),
-  y: -Math.floor(Math.random() * 5),
-  reverse: Math.random() < 0.5,
-  frame: Math.floor(Math.random() * sprites[spriteTypes.WAVES].frameCount),
-  mirror:  Math.random() < 0.5
-});
+const createWaves = () => {
+  const mirror = Math.random() < 0.5;
+  return {
+    sprite: spriteTypes.WAVES,
+    x: Math.floor(Math.random() * 5) + (mirror ? 2 : 0),
+    y: -Math.floor(Math.random() * 5),
+    reverse: Math.random() < 0.5,
+    frame: Math.floor(Math.random() * sprites[spriteTypes.WAVES].frameCount),
+    mirror
+  };
+};
 
 export default createWaves;
