66c61a9120e9351118afda28d54d41094570120e
Dirk Plate <email@dplate.de>
2025-08-02 12:51:59 +0200

    Update android version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "de.dplate.schiffbruch"
         minSdk = 21
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
     }
 
     buildTypes {
